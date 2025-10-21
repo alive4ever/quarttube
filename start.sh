c49d96ce8b535335af74af891cb6fd24115f94ae
alive4ever <alive4ever@users.noreply.github.com>
2025-10-21 06:03:31 +0000

    start: use watchfiles instead of gunicorn reload
    Use `watchfiles` instead of `gunicorn reload` to monitor settings file
    for changes.
    
    Also modify the startup script example to create an empty settings.ini
    file if the data directory doesn't have settings.ini inside.
    
    Also fix naming for media storage to `media_storage.db` after changing
    the storage backend to sqlite3 database.

--- start.sh
+++ start.sh
@@ -1,9 +1,12 @@
 . ~/venv/bin/activate
 cd ~/quarttube
 if [ -d ./data ] ; then
-	rm -f ./data/media_storage.json
+	rm -f ./data/media_storage.db
 	rm -f ./data/cookies.txt
 	rm -f ./data/ytcfg.json
 fi
-#gunicorn --reload-extra-file 'data/settings.ini' --reload --timeout 50 -k uvicorn_worker.UvicornWorker -b localhost:5000 quarttube:app
+if ! [ -f ./data/settings.ini ] ; then
+	touch ./data/settings.ini
+fi
+#watchfiles 'gunicorn --timeout 50 -k uvicorn_worker.UvicornWorker -b localhost:5000 quarttube:app' 'data/settings.ini'
 uvicorn --reload --reload-include 'data/settings.ini' --host 'localhost' --port 5000 --no-access-log quarttube:app
